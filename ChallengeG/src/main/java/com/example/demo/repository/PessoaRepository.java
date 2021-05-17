8ff59dfa19b87fd4024678f7e3262bb304db8072
EdgarVieira2 <68427687+EdgarVieira2@users.noreply.github.com>
2021-05-17 13:32:50 -0300

    Update PessoaRepository.java

--- ChallengeG/src/main/java/com/example/demo/repository/PessoaRepository.java
+++ ChallengeG/src/main/java/com/example/demo/repository/PessoaRepository.java
@@ -7,5 +7,7 @@ import com.example.demo.model.Pessoa;
 
 @Repository
 public interface PessoaRepository extends MongoRepository<Pessoa, String> {
+  
+  public Optional<List<Pessoa>> findByDocumento(String documento);
 
 }
